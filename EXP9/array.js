47ca3e8acecfe6f966cf56b59ebdbd17fffb5b76
jay7984 <jay942961@gmail.com>
2023-10-20 11:16:02 +0530

    new file:   .vscode/launch.json 	modified:   EXP3/FRAMES/chapter frame file.html 	modified:   EXP3/MAP/MAP exp3.html 	new file:   EXP5/20bec045_5.zip 	new file:   EXP8/NumToString.html 	new file:   EXP8/ReverseNum.html 	new file:   EXP8/String.html 	new file:   EXP9/array.html 	new file:   EXP9/array.js 	new file:   EXP9/suffix.html 	new file:   EXP9/suffix.js 	modified:   HTML file.html 	new file:   animation.html 	new file:   circle.html 	modified:   html.html 	new file:   test.html

---
+++ EXP9/array.js
@@ -0,0 +1,6 @@
+var a=[1,2];
+var b=[2,3];
+ c = a.concat(b);
+
+ d =  [...new Set(c)];
+document.write(d);
---
+++ EXP9/suffix.js
@@ -0,0 +1,14 @@
+num = window.prompt("Enter number :");
+
+            if(num == 1){
+                document.write(num + "st");
+            }
+            else if(num == 2){
+                document.write(num + "nd");
+            }
+            else if(num == 3){
+                document.write(num + "rd");
+            }
+            else{
+                document.write(num + "th");
+            }
